406a13e79dcc16eeee3e6a6e170151dfa1caf880
Shailendra Pratap <2k24.csaiml1c.2412673@gmail.com>
2026-07-16 13:55:29 +0530

    Time: 0 ms (100%), Space: 11.3 MB (62.18%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> s = heights;     
+
+        sort(heights.begin(), heights.end());
+
+        int count = 0;
+
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != s[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
