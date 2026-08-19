d1f27bb80306f32c82cc9b61f2851dd3f3427d19
Shailendra Pratap <2k24.csaiml1c.2412673@gmail.com>
2026-08-19 11:48:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        int left=1,right=x,ans=1;
+        while(left<=right){
+            long long mid=left+(right-left)/2;
+            if(mid*mid<=x){
+                ans = mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};
