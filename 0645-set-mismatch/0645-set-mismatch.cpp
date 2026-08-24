31f28acdab2685caef2023e9eb3857348050d32c
Shailendra Pratap <2k24.csaiml1c.2412673@gmail.com>
2026-08-24 22:06:45 +0530

    Time: 3 ms (46.39%), Space: 26.3 MB (37.01%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        int duplicate = 0, missing = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2)
+                duplicate = i;
+            else if (freq[i] == 0)
+                missing = i;
+        }
+
+        return {duplicate, missing};
+    }
+};
