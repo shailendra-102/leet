88d3d65e5c12e3e8e34c08088d99d5ec85fe1fd3
Shailendra Pratap <2k24.csaiml1c.2412673@gmail.com>
2026-09-23 12:28:35 +0530

    Time: 0 ms (100%), Space: 8.8 MB (74.26%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==1){
-            return true;
-        }
-        if(n<=0 || n%3 != 0)
-        return false;
-        return isPowerOfThree(n/3);
+        return n>0 && 1162261467 % n == 0;
     }
 };
